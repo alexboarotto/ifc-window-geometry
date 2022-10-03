0f5b05adad6f203784741570b323351fe542c4aa
Alexboa <alexboarotto@gmail.com>
2022-10-03 12:54:16 +0200

    removed heading print

--- main.py
+++ main.py
@@ -3,47 +3,32 @@ from modules.ifc_window_type_partitioning_enum import IfcWindowTypePartitioningE
 from modules.get_props import get_props as props
 from modules.generate_geometry import generate_geometry as geo
 
-def print_heading(text):
-  print("============================================")
-  print(text)
-  print("============================================")
 
 def create_geometry(window_type = IfcWindowTypePartitioningEnum.NOTDEFINED, OHeight = 500, OWidth = 500):
   if window_type not in [wType.name for wType in list(IfcWindowTypePartitioningEnum)]:
     raise Exception("Window Type not valid.")
 
   if window_type == IfcWindowTypePartitioningEnum(1).name:
-    print_heading(window_type)
     create_double_panel_horizontal(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(2).name:
-    print_heading(window_type)
     create_double_panel_vertical(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(3).name:
-    print_heading(window_type)
     create_single_panel(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(4).name:
-    print_heading(window_type)
     create_triple_panel_bottom(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(5).name:
-    print_heading(window_type)
     create_triple_panel_horizontal(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(6).name:
-    print_heading(window_type)
     create_triple_panel_left(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(7).name:
-    print_heading(window_type)
     create_triple_panel_right(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(8).name:
-    print_heading(window_type)
     create_triple_panel_top(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(9).name:
-    print_heading(window_type)
     create_triple_panel_vertical(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(10).name:
-    print_heading(window_type)
     create_userdefined(OHeight, OWidth)
   elif window_type == IfcWindowTypePartitioningEnum(11).name:
-    print_heading(window_type)
     create_undefined(OHeight, OWidth)
 
 def create_single_panel(OHeight, OWidth):
