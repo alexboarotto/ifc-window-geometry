bf5c61fbe014be5dc268b0d873fc4fc09b843e68
Alexboa <alexboarotto@gmail.com>
2022-10-03 11:49:59 +0200

    added mullion and transom properties

--- modules/get_props.py
+++ modules/get_props.py
@@ -3,13 +3,30 @@ import numpy
 
 
 class get_props:
-    def get_lining_props(LiningThickness = 10, LiningDepth = 10, LiningOffset = 0, LiningToPanelOffsetX = 10, LiningToPanelOffsetY = 0):
+    def get_lining_props(
+        LiningThickness = 10,
+        LiningDepth = 10,
+        LiningOffset = 0,
+        LiningToPanelOffsetX = 0,
+        LiningToPanelOffsetY = 0,
+        MullionThickness = 0,
+        FirstMullionOffset = 0,
+        SecondMullionOffset = 0,
+        TransomThickness = 0,
+        FirstTransomOffset = 0,
+        SecondTransomOffset = 0):
         return {
             "LiningThickness": LiningThickness,
             "LiningOffset": LiningOffset,
             "LiningDepth": LiningDepth,
             "LiningToPanelOffsetX": LiningToPanelOffsetX,
             "LiningToPanelOffsetY": LiningToPanelOffsetY,
+            "MullionThickness": MullionThickness,
+            "FirstMullionOffset": FirstMullionOffset,
+            "SecondMullionOffset": SecondMullionOffset,
+            "TransomThickness": TransomThickness,
+            "FirstTransomOffset": FirstTransomOffset,
+            "SecondTransomOffset": SecondTransomOffset
         }
 
     def get_panel_props(PanelPosition = numpy.array([0,0,0]), FrameDepth = 10, FrameThickness = 10):
